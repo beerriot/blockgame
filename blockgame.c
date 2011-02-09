049495c9ba1bd924699501d0488651718acb16ac
Bryan Fink <bryan.fink@gmail.com>
2011-02-08 23:37:47 -0500

    exchange interrupt button reader for polled reader
    fixes button bouncing and missed button presses

--- blockgame.c
+++ blockgame.c
@@ -28,6 +28,14 @@
 #define B_RIGHT_INT  (1<<PCINT11)
 #define B_SELECT_INT (1<<PCINT13)
 
+// living state for the button reader
+struct button_states {
+    // the stable state (repeated agreeing reads) of the buttons
+    uint8_t stable;
+    // the most recent read of the buttons
+    uint8_t last_read;
+};
+
 // last button state
 volatile uint8_t button_states = 0;
 
@@ -36,21 +44,6 @@ volatile uint8_t buttons_pushed = 0;
 
 // wheter or not the animate timer has clicked
 volatile int animate = 0;
-
-ISR(PCINT1_vect) {
-    uint8_t fresh_buttons = PINC & (B_LEFT|B_DOWN|B_UP|B_RIGHT|B_SELECT);
-    
-    if (!buttons_pushed) {
-        // only allow button presses when asked
-        // and require a button to be released before activating again
-        buttons_pushed = (button_states ^ fresh_buttons) & fresh_buttons;
-        button_states = fresh_buttons;
-    } else {
-        // only *clear* states,
-        // do not set them until pushed has been cleared
-        button_states &= fresh_buttons;
-    }
-}
 
 ISR(TIMER0_COMPA_vect) {
     // time to cycle animations
@@ -86,12 +79,6 @@ void boot_pins() {
 	  
     // turn on the internal resistors for the pins
     PORTC |= (B_LEFT|B_DOWN|B_UP|B_RIGHT|B_SELECT);
-
-    // enable bin change mask registers
-    PCMSK1 |= (B_LEFT_INT|B_DOWN_INT|B_UP_INT|B_RIGHT_INT|B_SELECT_INT);
-
-    // enable Pin Change Interrupt 1
-    PCICR |= (1<<PCIE1);
 }
 
 // configure the animation timer at boot
@@ -112,8 +99,35 @@ void boot_timer() {
   TIMSK0 |= (1<<OCIE0A);
 }
 
+// clear out all state for the button reader
+void clear_button_state(struct button_states* state) {
+    state->stable = 0;
+    state->last_read = 0;
+}
+
+// check the state of the buttons, returns a mask of
+// what buttons are now pushed that weren't before
+uint8_t read_buttons(struct button_states* state) {
+    // get a fresh read
+    uint8_t fresh = PINC & (B_LEFT|B_DOWN|B_UP|B_RIGHT|B_SELECT);
+
+    // factor out bounces by sampling twice before determining
+    // what is actually pressed
+    uint8_t pressed = state->last_read & fresh;
+
+    // find out what buttons are pushed now that weren't before
+    uint8_t newly = (state->stable ^ pressed) & pressed;
+
+    // update state
+    state->last_read = fresh;
+    state->stable = pressed;
+
+    // reply with newly-pushed buttons
+    return newly;
+}
+
 // handle any directional button pushes
-void move_cursor(int* row, int* column) {
+void move_cursor(uint8_t buttons_pushed, int* row, int* column) {
     if (buttons_pushed & B_UP)
         (*row)--;
     if (buttons_pushed & B_DOWN)
@@ -128,7 +142,9 @@ void move_cursor(int* row, int* column) {
 }
 
 // handle a select button push
-void do_select(char board[3][10], int row, int column) {
+void do_select(uint8_t buttons_pushed,
+               char board[3][10],
+               int row, int column) {
     if (buttons_pushed & B_SELECT) {
         if (board[row][column] >= 'a')
             board[row][column] = board[row][column]-('a'-'A');
@@ -153,26 +169,34 @@ void write_board(char board[3][10]) {
     write_row(board[2]);
 }
 
-void show_button(uint8_t button, char label) {
-    lcd_write_data((button_states & button) ? label : ' ');
+void show_button(struct button_states button_state,
+                 uint8_t button, char label) {
+    lcd_write_data((button_state.stable & button) ? label : ' ');
     lcd_write_data(' ');
 }
 
-void show_buttons() {
+void show_buttons(struct button_states button_state) {
     lcd_line_four();
-    show_button(B_LEFT,   'L');
-    show_button(B_DOWN,   'D');
-    show_button(B_UP,     'U');
-    show_button(B_RIGHT,  'R');
-    show_button(B_SELECT, 'S');
+    show_button(button_state, B_LEFT,   'L');
+    show_button(button_state, B_DOWN,   'D');
+    show_button(button_state, B_UP,     'U');
+    show_button(button_state, B_RIGHT,  'R');
+    show_button(button_state, B_SELECT, 'S');
 }
 
 int main() {
 
     // row and column of the cursor
     int row = 0, column = 0;
+    // the playing board
     char board[3][10];
+    // read state
+    struct button_states button_state;
+    // latest new presses
+    uint8_t pressed_buttons;
+
     boot_board(board);
+    clear_button_state(&button_state);
 
     boot_lcd();
     boot_pins();
@@ -181,18 +205,20 @@ int main() {
     sei(); //enable interrupts
     animate = 1; // draw the board the first time
     while(1) {
-        if(buttons_pushed) {
-            move_cursor(&row, &column);
-            do_select(board, row, column);
-            buttons_pushed = 0;
-        }
-        
         if (animate) {
+            pressed_buttons = read_buttons(&button_state);
+
+            if(pressed_buttons) {
+                move_cursor(pressed_buttons, &row, &column);
+                do_select(pressed_buttons, board, row, column);
+                buttons_pushed = 0;
+            }
+        
             write_board(board);
             animate = 0;
-        }
 
-        show_buttons(); // debug
+            show_buttons(button_state); // debug
+        }
     }
     return 0;
 }
